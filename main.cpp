81ee4252156dd3fbac814463e11d753c7263e98e
Shaik Abdul Thouhid <shaikabdulthouhid.01@gmail.com>
2022-09-28 01:01:58 +0530

    first commit

---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <vector>
+#include <windows.h>
+
+[[__nodiscard__]] int main(int argc, char const *argv[])
+{
+	std::vector<int> a{};
+	std::cout << "Test" << std::endl;
+	return 0;
+}
